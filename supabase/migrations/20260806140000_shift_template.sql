91cb0471c127362b9f245d9c9ef842e811276a34
Dario Soffiati <dario.soffiati@gmail.com>
2026-09-09 21:38:17 +0200

    Migrazione shift_template: aggiunta colonna duration_weeks (allineata a prod)

--- supabase/migrations/20260806140000_shift_template.sql
+++ supabase/migrations/20260806140000_shift_template.sql
@@ -7,5 +7,8 @@ ALTER TABLE shifts
   ADD COLUMN template_weekday SMALLINT,
   ADD COLUMN template_shift_id TEXT;
 
+-- Presente solo su installazioni gia' esistenti (aggiunta manualmente su prod).
+ALTER TABLE shifts ADD COLUMN IF NOT EXISTS duration_weeks INTEGER;
+
 CREATE INDEX idx_shifts_is_template ON shifts(is_template);
 CREATE INDEX idx_shifts_template_shift_id ON shifts(template_shift_id);
